6761ea046da5dc548232cd5d43f4ebe12845b994
kristian1402 <72245684+kristian1402@users.noreply.github.com>
2024-04-16 23:21:10 +0200

    Update DetectHits.cs

--- Assets/Scripts/DetectHits.cs
+++ Assets/Scripts/DetectHits.cs
@@ -3,69 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-
 public class DetectHits : MonoBehaviour
 {
-    
-
     public int hits;
     public GameObject[] targets; //Array for the prefabs
-
-
-    private int fileCounter = 1; 
-    string filename = "";
-   
+    string filename;
+    float startTime;
+    bool savingData = false;
+    float saveDelay = 60f; // Time delay before saving data in seconds
 
     // Start is called before the first frame update
     void Start()
     {
-        filename = Application.dataPath + "/TxtFiles/Timestamps"+fileCounter+".txt";
+        filename = Application.dataPath + "/TxtFiles/TimestampsTest.txt";
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.CompareTag("RedBullet") && gameObject.CompareTag("RedTarget")){
-            hits+=1;
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
-            spawnTarget();
-           
-
-        }
-
-        if(collision.gameObject.CompareTag("BlueBullet") && gameObject.CompareTag("BlueTarget")){
-            hits+=1;
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
-            spawnTarget();
-       
-        }
-
-        if(collision.gameObject.CompareTag("GreenBullet") && gameObject.CompareTag("GreenTarget")){
-            hits+=1;
-            Destroy(collision.gameObject);
-            Destroy(gameObject);
-            spawnTarget();
-           
+        if (!savingData && Time.time >= saveDelay)
+        {
+            savingData = true;
+            saveData();
+            Debug.Log("SAVED");
         }
     }
 
-    void spawnTarget(){
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RedBullet") && gameObject.CompareTag("RedTarget"))
+        {
+            hits += 1;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            spawnTarget();
+        }
 
+        if (collision.gameObject.CompareTag("BlueBullet") && gameObject.CompareTag("BlueTarget"))
+        {
+            hits += 1;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            spawnTarget();
+        }
 
-        if(targets.Length > 0){
+        if (collision.gameObject.CompareTag("GreenBullet") && gameObject.CompareTag("GreenTarget"))
+        {
+            hits += 1;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            spawnTarget();
+        }
+    }
+
+    void spawnTarget()
+    {
+        if (targets.Length > 0)
+        {
             float randomX = Random.Range(-7f, 7f);
             float randomY = Random.Range(0f, 5f);
             float randomZ = Random.Range(1f, 13f);
             Vector3 randomPosition = new Vector3(randomX, randomY, randomZ);
-            
+
             float randomScale = Random.Range(0.35f, 1f);
-           
 
             int randomIndex = Random.Range(0, targets.Length);
             GameObject spawnedTarget = Instantiate(targets[randomIndex], randomPosition, Quaternion.identity);
@@ -73,32 +74,26 @@ public class DetectHits : MonoBehaviour
             spawnedTarget.transform.localScale = Vector3.one * randomScale;
 
             WriteTimestampToFile(Time.time);
-           
-    
-            
         }
-
     }
 
-   void WriteTimestampToFile(float timestamp)
-{
-    using (TextWriter tw = new StreamWriter(filename, true))
+    void WriteTimestampToFile(float timestamp)
     {
-        
-        tw.WriteLine(timestamp);
-        
+        using (TextWriter tw = new StreamWriter(filename, true))
+        {
+            tw.WriteLine(timestamp);
+        }
     }
 
-    fileCounter ++;
-}
+    void saveData()
+    {
+        string timeStamp = System.DateTime.Now.ToString("ddMMyyyy_HHmmss");
+        string newFilename = Application.dataPath + "/TxtFiles/Timestamps_" + timeStamp + ".txt";
 
-       
-    
+        File.Move(filename, newFilename);
 
-    void OnApplicationQuit(){
-        Debug.Log("Application Quit");
+        Debug.Log("File Saved");
+
+        UnityEditor.EditorApplication.isPlaying = false;
     }
-
-
-    
 }
